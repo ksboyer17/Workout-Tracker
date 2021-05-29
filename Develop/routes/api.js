0d5f7981a658fe41102b870b68d6fb693d1a0c35
Kenny Boyer <ksboyerii@gmail.com>
2021-05-29 09:08:56 -0500

    working on routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,2 +1,10 @@
 const router = router = require("express").Router();
 const workout = require("../models/workout")
+
+applicationCache.post("api/workouts", function (req, res){
+    workout.find()
+    .then (data => res.json(data))
+    .catch(err => {
+        res.json(err)
+    })
+})
